3cdc2c442992d8ed0470380c1d7569d6f0f251d6
Samuel Eras <sa.eras@gmx.de>
2025-10-16 19:04:27 +0200

    feat: add endpoint

--- src/main/java/com/samuel/springboot_test/HelloWorldController.java
+++ src/main/java/com/samuel/springboot_test/HelloWorldController.java
@@ -1,14 +1,29 @@
 package com.samuel.springboot_test;
 
+import com.samuel.springboot_test.domain.Author;
+import com.samuel.springboot_test.services.AuthorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    private final AuthorService authorService;
+
+    public HelloWorldController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
     @GetMapping(path = "/hello")
     public String helloWorld(){
-        return "Hello World";
+        Author author = Author.builder()
+                .name("Samuel")
+                .age(25)
+                .build();
+
+        authorService.createAuthor(author);
+
+        return author.toString();
     }
 
 }
