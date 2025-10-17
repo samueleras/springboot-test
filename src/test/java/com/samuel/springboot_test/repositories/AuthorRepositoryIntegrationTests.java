a7afd8994844764b85b0c78e63870ab6a01d8a72
Samuel Eras <sa.eras@gmx.de>
2025-10-17 13:44:22 +0200

    refactor: rename AuthorDaoImplIntegrationTests to AuthorRepositoryIntegrationTests

--- src/test/java/com/samuel/springboot_test/repositories/AuthorRepositoryIntegrationTests.java
+++ src/test/java/com/samuel/springboot_test/repositories/AuthorRepositoryIntegrationTests.java
@@ -8,35 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-/*
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-public class AuthorDaoImplIntegrationTests {
+public class AuthorRepositoryIntegrationTests {
 
-    private AuthorDaoImpl underTest;
+    private AuthorRepository underTest;
 
     @Autowired
-    public AuthorDaoImplIntegrationTests(AuthorDaoImpl underTest) {
+    public AuthorRepositoryIntegrationTests(AuthorRepository underTest) {
         this.underTest = underTest;
     }
 
     @Test
-    public void testThatAuthorCanBeCreatedAndRecalled(){
+    public void testThatAuthorCanBeCreatedAndRecalled() {
 
         Author author = TestDataUtil.createTestAuthorA();
 
-        Author savedAuthor = underTest.create(author);
-        Optional<Author> result = underTest.findOne(savedAuthor.getAuthorId());
+        Author savedAuthor = underTest.save(author);
+        Optional<Author> result = underTest.findById(savedAuthor.getId());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(savedAuthor);
     }
 
+    /*
     @Test
-    public void testThatMultipleAuthorsCanBeCreatedAndRecalled(){
+    public void testThatMultipleAuthorsCanBeCreatedAndRecalled() {
         Author authorA = TestDataUtil.createTestAuthorA();
         Author authorB = TestDataUtil.createTestAuthorB();
         Author authorC = TestDataUtil.createTestAuthorC();
@@ -46,6 +46,6 @@ public class AuthorDaoImplIntegrationTests {
         Author savedAuthorC = underTest.create(authorC);
         List<Author> results = underTest.findMany();
         assertThat(results).isNotNull().hasSize(3).containsExactly(savedAuthorA, savedAuthorB, savedAuthorC);
-    }
+    }*/
 
-}*/
+}
