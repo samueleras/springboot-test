c21d0324fa3cb84d92097d3bb9bd887be4562438
Samuel Eras <sa.eras@gmx.de>
2025-10-20 12:20:44 +0200

    feat: create BookDto class

---
+++ src/main/java/com/samuel/springboot_test/domain/dto/BookDto.java
@@ -0,0 +1,24 @@
+package com.samuel.springboot_test.domain.dto;
+
+import com.samuel.springboot_test.domain.AuthorEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookDto {
+
+    private String isbn;
+
+    private String title;
+
+    private AuthorEntity authorEntity;
+
+}
