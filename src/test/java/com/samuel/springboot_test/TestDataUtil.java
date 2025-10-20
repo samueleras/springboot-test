c72dedb0afaaec485d8146f210b12838b0091c1b
Samuel Eras <sa.eras@gmx.de>
2025-10-20 15:01:36 +0200

    feat(test): extend TestDataUtil to allow creation of test objects as dto

--- src/test/java/com/samuel/springboot_test/TestDataUtil.java
+++ src/test/java/com/samuel/springboot_test/TestDataUtil.java
@@ -2,11 +2,17 @@ package com.samuel.springboot_test;
 
 import com.samuel.springboot_test.domain.AuthorEntity;
 import com.samuel.springboot_test.domain.BookEntity;
+import com.samuel.springboot_test.domain.dto.AuthorDto;
+import com.samuel.springboot_test.domain.dto.BookDto;
+
 
 public final class TestDataUtil {
 
-    private TestDataUtil(){}
+    private TestDataUtil() {}
 
+    // ==========================
+    // Author Entities
+    // ==========================
     public static AuthorEntity createTestAuthorA() {
         return AuthorEntity.builder()
                 .name("Samuel")
@@ -28,11 +34,38 @@ public final class TestDataUtil {
                 .build();
     }
 
+    // ==========================
+    // Author DTOs
+    // ==========================
+    public static AuthorDto createTestAuthorDtoA() {
+        return AuthorDto.builder()
+                .name("Samuel")
+                .age(25)
+                .build();
+    }
+
+    public static AuthorDto createTestAuthorDtoB() {
+        return AuthorDto.builder()
+                .name("Gerta")
+                .age(26)
+                .build();
+    }
+
+    public static AuthorDto createTestAuthorDtoC() {
+        return AuthorDto.builder()
+                .name("Thomas")
+                .age(18)
+                .build();
+    }
+
+    // ==========================
+    // Book Entities
+    // ==========================
     public static BookEntity createTestBookA(AuthorEntity authorEntity) {
         return BookEntity.builder()
                 .isbn("AAAAAAAA")
                 .title("Harry Potter 1")
-                .authorEntity(authorEntity)
+                .author(authorEntity)
                 .build();
     }
 
@@ -40,7 +73,7 @@ public final class TestDataUtil {
         return BookEntity.builder()
                 .isbn("BBBBBBBB")
                 .title("Harry Potter 2")
-                .authorEntity(authorEntity)
+                .author(authorEntity)
                 .build();
     }
 
@@ -48,7 +81,34 @@ public final class TestDataUtil {
         return BookEntity.builder()
                 .isbn("CCCCCCC")
                 .title("Harry Potter 3")
-                .authorEntity(authorEntity)
+                .author(authorEntity)
+                .build();
+    }
+
+    // ==========================
+    // Book DTOs
+    // ==========================
+    public static BookDto createTestBookDtoA(AuthorDto authorDto) {
+        return BookDto.builder()
+                .isbn("AAAAAAAA")
+                .title("Harry Potter 1")
+                .author(authorDto)
+                .build();
+    }
+
+    public static BookDto createTestBookDtoB(AuthorDto authorDto) {
+        return BookDto.builder()
+                .isbn("BBBBBBBB")
+                .title("Harry Potter 2")
+                .author(authorDto)
+                .build();
+    }
+
+    public static BookDto createTestBookDtoC(AuthorDto authorDto) {
+        return BookDto.builder()
+                .isbn("CCCCCCC")
+                .title("Harry Potter 3")
+                .author(authorDto)
                 .build();
     }
 }
