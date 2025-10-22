2c1f5efaca1a68d934ff27f03ebb17bb056150a5
Samuel Eras <sa.eras@gmx.de>
2025-10-22 22:38:08 +0200

    feat(service): add method in AuthorService interface for fetching all authors

--- src/main/java/com/samuel/springboot_test/services/AuthorService.java
+++ src/main/java/com/samuel/springboot_test/services/AuthorService.java
@@ -3,7 +3,10 @@ package com.samuel.springboot_test.services;
 import com.samuel.springboot_test.domain.AuthorEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+    List<AuthorEntity> listAuthors();
 }
