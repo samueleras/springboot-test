b1ff0c95cd1a869c13a93075fe6c53ea0d610d47
Samuel Eras <sa.eras@gmx.de>
2025-10-22 22:39:05 +0200

    feat(controller): expose endpoint for fetching a list of all authors

--- src/main/java/com/samuel/springboot_test/controllers/AuthorController.java
+++ src/main/java/com/samuel/springboot_test/controllers/AuthorController.java
@@ -6,9 +6,13 @@ import com.samuel.springboot_test.mappers.Mapper;
 import com.samuel.springboot_test.services.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -27,6 +31,14 @@ public class AuthorController {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/authors")
+    public ResponseEntity<List<AuthorDto>> listAuthors() {
+        List<AuthorEntity> authorEntities = authorService.listAuthors();
+        return new ResponseEntity<>(authorEntities.parallelStream()
+                .map(authorMapper::mapTo)
+                .collect(Collectors.toList()), HttpStatus.OK);
     }
 
 }
